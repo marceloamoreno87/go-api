2b1d0fc5ddaf7aa398ec81461ad28a936d1f3300
Marcelo Moreno <marcelo.am@itaccept.com.br>
2024-01-13 00:38:00 -0300

    init

--- izimoney/go.mod
+++ izimoney/go.mod
@@ -1,3 +1,5 @@
 module github.com/marceloamoreno/izimoney
 
 go 1.22rc1
+
+require github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
--- izimoney/main.go
+++ izimoney/main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"os"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler)
-
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	})
 	http.ListenAndServe(":3000", mux)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Shell:", os.Getenv("IZIMONEY_DB_HOST"))
-
-	fmt.Fprintf(w, "Hello World")
 }
