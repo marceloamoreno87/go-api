a49b7ba92e8a03b9e32ac89e8308348a6dfba2d1
Marcelo Moreno <marcelo.am@itaccept.com.br>
2024-01-15 17:11:49 -0300

    first api

--- api/internal/infra/webserver/server.go
+++ api/internal/infra/webserver/server.go
@@ -14,7 +14,7 @@ import (
 
 func StartServer() {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+	loggerMiddleware(r)
 	loadRoutes(r)
 	http.ListenAndServe(":"+os.Getenv("PORT"), r)
 }
@@ -24,5 +24,21 @@ func loadRoutes(r *chi.Mux) {
 	route := routes.NewRoute(r, database.Db(), HandlerTools)
 	route.GetUserRoutes()
 	route.GetSwaggerRoutes()
+}
 
+func loggerMiddleware(r *chi.Mux) {
+	r.Use(middleware.Logger)
+}
+
+func corsMiddleware(r *chi.Mux) {
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 }
