33ebe3f4b848ebc5f8bb6a01f7103604b98f2af7
Marcelo Moreno <marcelo.am@itaccept.com.br>
2024-01-28 20:38:26 -0300

    fix test

--- api/test/unit/user.handler_test.go
+++ api/test/unit/user.handler_test.go
@@ -2,34 +2,19 @@ package unit
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func TestNewUserHandler(t *testing.T) {
-	assert.NotNil(t, nil)
-}
-
 func TestUserHandlerGetUser(t *testing.T) {
-	assert.NotNil(t, nil)
 }
 
 func TestUserHandlerGetUsers(t *testing.T) {
-
-	assert.NotNil(t, nil)
 }
 
 func TestUserHandlerCreateUser(t *testing.T) {
-
-	assert.NotNil(t, nil)
 }
 
 func TestUserHandlerUpdateUser(t *testing.T) {
-
-	assert.NotNil(t, nil)
 }
 
 func TestUserHandlerDeleteUser(t *testing.T) {
-
-	assert.NotNil(t, nil)
 }
